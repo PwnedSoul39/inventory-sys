a06267afaaee62258378a41bf9bc1c2b5a4c5599
BillyWilder44 <124748839+BillyWilder44@users.noreply.github.com>
2023-05-27 22:22:55 +0800

    accessory page added

--- resource/php/class/config.php
+++ resource/php/class/config.php
@@ -6,7 +6,11 @@
 
         public function con() {
             try {
+<<<<<<< Updated upstream
                 $this->pdo = new PDO('mysql:host=localhost:3307;dbname=db_inventory_sys',$this->user, $this->pass);
+=======
+                $this->pdo = new PDO('mysql:host=127.0.0.1:3306;dbname=inventory_sys',$this->user, $this->pass);
+>>>>>>> Stashed changes
             } catch (PDOException $e) {
                 die($e->getMessage());
             }
